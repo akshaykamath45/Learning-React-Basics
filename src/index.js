1c7c3805189befaf19bb0a4ebd6f0d7120201a8d
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-01-30 17:05:06 +0530

    Creating Element

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// // capitalize the first letter of your component's name.
+
+// function Greeting(){
+//     let name="Akshay";//JSX-->Using HTML in JavaScript--->return<h4>This is {name} and this is my first component.</h4>
+//     return <h4>This is Akshay and this is my first component.</h4>
+// }
+
+// ReactDOM.render(<Greeting></Greeting>,document.getElementById("root"));
 import React from "react";
 import ReactDOM from "react-dom";
 
-// capitalize the first letter of your component's name.
-
-function Greeting(){
-    return <h4>This is Akshay and this is my first component.</h4>
+const Greeting=() =>{
+    // return React.createElement("h1",{},'Hello World');
+    return React.createElement("div",{},React.createElement("h1",{},"hello peoples"))
 }
 
 ReactDOM.render(<Greeting></Greeting>,document.getElementById("root"));
